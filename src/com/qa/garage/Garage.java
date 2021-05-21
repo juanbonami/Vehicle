befe839b07954f4edefde3ab2c863077acaae28b
juan <juanbonami@gmail.com>
2021-05-20 22:52:47 -0500

    reconfigured adding method, first i created instances of the Vehicle class and passed in their values, then created add and delete method

--- src/com/qa/garage/Garage.java
+++ src/com/qa/garage/Garage.java
@@ -6,21 +6,44 @@ import java.util.List;
 public class Garage {
 	public static void main(String[] args) {
 		
-		bill(garageInventory());
+		Car c = new Car(4, 16000 , "red", 22, 25, 50.4);
+		Motorcycle m = new Motorcycle(120, 10500, "green", 23, 27, 30.3);
+		Rv r = new Rv(true, 32200, "grey", 18, 25, 60.1);
+		
+		
+		
+		addVehicle(c);
+		addVehicle(m);
+		addVehicle(r);
+		
+		bill(garage);
+		
 	}
 	
+	// garage 
+	static List<Vehicle> garage = new ArrayList<>();
+	
+	// returns price after tax
 	static void bill(List<Vehicle> vehicle) {
 		for (Vehicle v : vehicle) {
 			System.out.println(v.getSalesTax());
 		}
 	}
 	
-	static List<Vehicle> garageInventory() {
-		 List<Vehicle> garage = new ArrayList<>();
-		garage.add(new Car(4, 16000 , "red", 22, 25, 50.4));
-		garage.add(new Motorcycle(120, 10500, "green", 23, 27, 30.3));
-		garage.add(new Rv(true, 32200, "grey", 18, 25, 60.1));
-		
+	
+	
+	// adds an instance of a Vehicle to the List (Garage)
+	static List<Vehicle> addVehicle(Vehicle v) {
+		 garage.add(v);
+		return garage;
+	}
+	
+	
+	// deletes Vehicle
+	static List<Vehicle> deleteVehicle(Vehicle v) {
+		garage.remove(v);
 		return garage;
 	}
 }
+
+
